0800c4f841a376ddfadcab493f14daccc2754a48
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2022-12-25 19:45:07 -0500

    Initial Commit

---
+++ WDBFontOverwrite/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WDBFontOverwrite
+//
+//  Created by Zhuowei Zhang on 2022-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WDBFontOverwrite/WDBFontOverwriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDBFontOverwriteApp.swift
+//  WDBFontOverwrite
+//
+//  Created by Zhuowei Zhang on 2022-12-25.
+//
+
+import SwiftUI
+
+@main
+struct WDBFontOverwriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
