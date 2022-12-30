83323e8800fa13fef4dfd8ccd52e89dc832ea162
ginsudev <noahlittle199@gmail.com>
2022-12-31 10:05:02 +1100

    Improved UI and added ViewModel to store some logic in.

---
+++ WDBFontOverwrite/ContentView.ViewModel.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.ViewModel.swift
+//  WDBFontOverwrite
+//
+//  Created by Noah Little (@ginsudev) on 31/12/2022.
+//
+
+import Foundation
+
+extension ContentView {
+    struct FontToReplace {
+      var name: String
+      var postScriptName: String
+      var repackedPath: String
+    }
+    
+    struct CustomFont {
+      var name: String
+      var targetPath: String
+      var localPath: String
+      var alternativeTTCRepackMode: TTCRepackMode
+    }
+    
+    final class ViewModel: ObservableObject {
+        @Published var message = "Choose a font."
+        @Published var progress: Progress!
+        @Published var importPresented: Bool = false
+        @Published var importName: String = ""
+        @Published var importTTCRepackMode: TTCRepackMode = .woff2
+        
+        let fonts = [
+            FontToReplace(
+                name: "DejaVu Sans Condensed",
+                postScriptName: "DejaVuSansCondensed",
+                repackedPath: "DejaVuSansCondensed.woff2"
+            ),
+            FontToReplace(
+                name: "DejaVu Serif",
+                postScriptName: "DejaVuSerif",
+                repackedPath: "DejaVuSerif.woff2"
+            ),
+            FontToReplace(
+                name: "DejaVu Sans Mono",
+                postScriptName: "DejaVuSansMono",
+                repackedPath: "DejaVuSansMono.woff2"
+            ),
+            FontToReplace(
+                name: "Go Regular",
+                postScriptName: "GoRegular",
+                repackedPath: "Go-Regular.woff2"
+            ),
+            FontToReplace(
+                name: "Go Mono",
+                postScriptName: "GoMono",
+                repackedPath: "Go-Mono.woff2"
+            ),
+            FontToReplace(
+                name: "Fira Sans",
+                postScriptName: "FiraSans-Regular",
+                repackedPath: "FiraSans-Regular.2048.woff2"
+            ),
+            FontToReplace(
+                name: "Segoe UI",
+                postScriptName: "SegoeUI",
+                repackedPath: "segoeui.woff2"
+            ),
+            FontToReplace(
+                name: "Comic Sans MS",
+                postScriptName: "ComicSansMS",
+                repackedPath: "Comic Sans MS.woff2"
+            ),
+            FontToReplace(
+                name: "Choco Cooky",
+                postScriptName: "Chococooky",
+                repackedPath: "Chococooky.woff2"
+            ),
+        ]
+
+        let customFonts = [
+            CustomFont(
+                name: "SFUI.ttf",
+                targetPath: "/System/Library/Fonts/CoreUI/SFUI.ttf",
+                localPath: "CustomSFUI.woff2",
+                alternativeTTCRepackMode: .ttcpad
+            ),
+            CustomFont(
+                name: "Emoji",
+                targetPath: "/System/Library/Fonts/CoreAddition/AppleColorEmoji-160px.ttc",
+                localPath: "CustomAppleColorEmoji.woff2",
+                alternativeTTCRepackMode: .firstFontOnly
+            ),
+            CustomFont(
+                name: "PingFang.ttc",
+                targetPath: "/System/Library/Fonts/LanguageSupport/PingFang.ttc",
+                localPath: "CustomPingFang.woff2",
+                alternativeTTCRepackMode: .ttcpad
+            ),
+        ]
+    }
+}
--- WDBFontOverwrite/ContentView.swift
+++ WDBFontOverwrite/ContentView.swift
@@ -8,176 +8,159 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-struct FontToReplace {
-  var name: String
-  var postScriptName: String
-  var repackedPath: String
-}
-
-let fonts = [
-  FontToReplace(
-    name: "DejaVu Sans Condensed", postScriptName: "DejaVuSansCondensed",
-    repackedPath: "DejaVuSansCondensed.woff2"),
-  FontToReplace(
-    name: "DejaVu Serif", postScriptName: "DejaVuSerif", repackedPath: "DejaVuSerif.woff2"),
-  FontToReplace(
-    name: "DejaVu Sans Mono", postScriptName: "DejaVuSansMono", repackedPath: "DejaVuSansMono.woff2"
-  ),
-  FontToReplace(name: "Go Regular", postScriptName: "GoRegular", repackedPath: "Go-Regular.woff2"),
-  FontToReplace(name: "Go Mono", postScriptName: "GoMono", repackedPath: "Go-Mono.woff2"),
-  FontToReplace(
-    name: "Fira Sans", postScriptName: "FiraSans-Regular",
-    repackedPath: "FiraSans-Regular.2048.woff2"),
-  FontToReplace(name: "Segoe UI", postScriptName: "SegoeUI", repackedPath: "segoeui.woff2"),
-  FontToReplace(
-    name: "Comic Sans MS", postScriptName: "ComicSansMS", repackedPath: "Comic Sans MS.woff2"),
-  FontToReplace(
-    name: "Choco Cooky", postScriptName: "Chococooky", repackedPath: "Chococooky.woff2"),
-]
-
-struct CustomFont {
-  var name: String
-  var targetPath: String
-  var localPath: String
-  var alternativeTTCRepackMode: TTCRepackMode
-}
-
-let customFonts = [
-  CustomFont(
-    name: "SFUI.ttf", targetPath: "/System/Library/Fonts/CoreUI/SFUI.ttf",
-    localPath: "CustomSFUI.woff2", alternativeTTCRepackMode: .ttcpad),
-  CustomFont(
-    name: "Emoji", targetPath: "/System/Library/Fonts/CoreAddition/AppleColorEmoji-160px.ttc",
-    localPath: "CustomAppleColorEmoji.woff2", alternativeTTCRepackMode: .firstFontOnly),
-  CustomFont(
-    name: "PingFang.ttc", targetPath: "/System/Library/Fonts/LanguageSupport/PingFang.ttc",
-    localPath: "CustomPingFang.woff2", alternativeTTCRepackMode: .ttcpad),
-]
-
 struct ContentView: View {
-  @State private var message = "Choose a font."
-  @State private var progress: Progress!
-  @State private var importPresented: Bool = false
-  @State private var importName: String = ""
-  @State private var importTTCRepackMode: TTCRepackMode = .woff2
-  var body: some View {
-    ScrollView {
-      VStack {
-        Text(message).padding(8)
-        if let progress = progress {
-          ProgressView(progress)
-        }
-        ForEach(fonts, id: \.name) { font in
-          Button(action: {
-            message = "Running"
-            progress = Progress(totalUnitCount: 1)
-            overwriteWithFont(name: font.repackedPath, progress: progress) {
-              message = $0
-              progress = nil
+    @StateObject private var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                progressView
+                List {
+                    fontsList
+                    customFontsList
+                }
             }
-          }) {
-            Text(font.name).font(.custom(font.postScriptName, size: 18))
-          }.padding(8)
-        }
-        Divider()
-        ForEach(customFonts, id: \.name) { font in
-          Button(action: {
-            message = "Running"
-            progress = Progress(totalUnitCount: 1)
-            overwriteWithCustomFont(
-              name: font.localPath, targetName: font.targetPath, progress: progress
-            ) {
-              message = $0
-              progress = nil
+            .navigationTitle(viewModel.message)
+            .sheet(isPresented: $viewModel.importPresented) {
+                DocumentPicker(
+                    name: viewModel.importName,
+                    ttcRepackMode: viewModel.importTTCRepackMode) {
+                        viewModel.message = $0
+                    }
             }
-          }) {
-            Text("Custom \(font.name)")
-          }.padding(8)
-          Button(action: {
-            message = "Importing..."
-            importName = font.localPath
-            importTTCRepackMode = .woff2
-            importPresented = true
-          }) {
-            Text("Import custom \(font.name)")
-          }.padding(8)
-          Button(action: {
-            message = "Importing..."
-            importName = font.localPath
-            importTTCRepackMode = font.alternativeTTCRepackMode
-            importPresented = true
-          }) {
-            Text("Import custom \(font.name) with fix for .ttc")
-          }.padding(8)
-          Divider()
         }
-      }
-      Text(
-        "Custom fonts require font files that are ported for iOS.\nSee https://github.com/zhuowei/WDBFontOverwrite for details."
-      ).font(.system(size: 12))
+        .navigationViewStyle(.stack)
     }
-    .sheet(isPresented: $importPresented) {
-      DocumentPicker(name: importName, ttcRepackMode: importTTCRepackMode) {
-        message = $0
-      }
+    
+    @ViewBuilder
+    private var progressView: some View {
+        if let progress = viewModel.progress {
+            ProgressView(progress)
+        }
     }
-  }
+    
+    private var fontsList: some View {
+        Section {
+            ForEach(viewModel.fonts, id: \.name) { font in
+                Button {
+                    viewModel.message = "Running"
+                    viewModel.progress = Progress(totalUnitCount: 1)
+                    overwriteWithFont(name: font.repackedPath, progress: viewModel.progress) {
+                        viewModel.message = $0
+                        viewModel.progress = nil
+                    }
+                } label: {
+                    Text(font.name)
+                        .font(.custom(
+                            font.postScriptName,
+                            size: 18)
+                        )
+                }
+            }
+        } header: {
+            Text("Fonts")
+        }
+    }
+    
+    @ViewBuilder
+    private var customFontsList: some View {
+        ForEach(viewModel.customFonts, id: \.name) { font in
+            Section {
+                Button {
+                    viewModel.message = "Running"
+                    viewModel.progress = Progress(totalUnitCount: 1)
+                    overwriteWithCustomFont(
+                        name: font.localPath,
+                        targetName: font.targetPath,
+                        progress: viewModel.progress
+                    ) {
+                        viewModel.message = $0
+                        viewModel.progress = nil
+                    }
+                } label: {
+                    Text("Custom \(font.name)")
+                }
+                Button {
+                    viewModel.message = "Importing..."
+                    viewModel.importName = font.localPath
+                    viewModel.importTTCRepackMode = .woff2
+                    viewModel.importPresented = true
+                } label: {
+                    Text("Import custom \(font.name)")
+                }
+                Button {
+                    viewModel.message = "Importing..."
+                    viewModel.importName = font.localPath
+                    viewModel.importTTCRepackMode = font.alternativeTTCRepackMode
+                    viewModel.importPresented = true
+                } label: {
+                    Text("Import custom \(font.name) with fix for .ttc")
+                }
+            } header: {
+                Text(font.name)
+            }
+        }
+        Text(
+            "Custom fonts require font files that are ported for iOS.\nSee https://github.com/zhuowei/WDBFontOverwrite for details."
+        )
+        .font(.system(size: 12))
+    }
 }
 
 class WDBImportCustomFontPickerViewControllerDelegate: NSObject, UIDocumentPickerDelegate {
-  let name: String
-  let ttcRepackMode: TTCRepackMode
-  let completion: (String) -> Void
-  init(name: String, ttcRepackMode: TTCRepackMode, completion: @escaping (String) -> Void) {
-    self.name = name
-    self.ttcRepackMode = ttcRepackMode
-    self.completion = completion
-  }
-  func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL])
-  {
-    guard urls.count == 1 else {
-      completion("import one file at a time")
-      return
+    let name: String
+    let ttcRepackMode: TTCRepackMode
+    let completion: (String) -> Void
+    init(name: String, ttcRepackMode: TTCRepackMode, completion: @escaping (String) -> Void) {
+        self.name = name
+        self.ttcRepackMode = ttcRepackMode
+        self.completion = completion
     }
-    DispatchQueue.global(qos: .userInteractive).async {
-      let fileURL = urls[0]
-      let documentDirectory = FileManager.default.urls(
-        for: .documentDirectory, in: .userDomainMask)[0]
-      let targetURL = documentDirectory.appendingPathComponent(self.name)
-      let success = importCustomFontImpl(
-        fileURL: fileURL, targetURL: targetURL, ttcRepackMode: self.ttcRepackMode)
-      DispatchQueue.main.async {
-        self.completion(success ?? "Imported")
-      }
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL])
+    {
+        guard urls.count == 1 else {
+            completion("import one file at a time")
+            return
+        }
+        DispatchQueue.global(qos: .userInteractive).async {
+            let fileURL = urls[0]
+            let documentDirectory = FileManager.default.urls(
+                for: .documentDirectory, in: .userDomainMask)[0]
+            let targetURL = documentDirectory.appendingPathComponent(self.name)
+            let success = importCustomFontImpl(
+                fileURL: fileURL, targetURL: targetURL, ttcRepackMode: self.ttcRepackMode)
+            DispatchQueue.main.async {
+                self.completion(success ?? "Imported")
+            }
+        }
     }
-  }
-  func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-    completion("Cancelled")
-  }
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        completion("Cancelled")
+    }
 }
 
 // https://capps.tech/blog/read-files-with-documentpicker-in-swiftui
 struct DocumentPicker: UIViewControllerRepresentable {
-  let controllerDelegate: WDBImportCustomFontPickerViewControllerDelegate
-  init(name: String, ttcRepackMode: TTCRepackMode, completion: @escaping (String) -> Void) {
-    controllerDelegate = WDBImportCustomFontPickerViewControllerDelegate(
-      name: name, ttcRepackMode: ttcRepackMode, completion: completion)
-  }
-  func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
-    print("make ui view controller?")
-    let pickerViewController = UIDocumentPickerViewController(
-      forOpeningContentTypes: [
-        UTType.font, UTType(filenameExtension: "woff2", conformingTo: .font)!,
-      ], asCopy: true)
-    pickerViewController.delegate = self.controllerDelegate
-    return pickerViewController
-  }
-  func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context)
-  {}
+    let controllerDelegate: WDBImportCustomFontPickerViewControllerDelegate
+    init(name: String, ttcRepackMode: TTCRepackMode, completion: @escaping (String) -> Void) {
+        controllerDelegate = WDBImportCustomFontPickerViewControllerDelegate(
+            name: name, ttcRepackMode: ttcRepackMode, completion: completion)
+    }
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        print("make ui view controller?")
+        let pickerViewController = UIDocumentPickerViewController(
+            forOpeningContentTypes: [
+                UTType.font, UTType(filenameExtension: "woff2", conformingTo: .font)!,
+            ], asCopy: true)
+        pickerViewController.delegate = self.controllerDelegate
+        return pickerViewController
+    }
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context)
+    {}
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
