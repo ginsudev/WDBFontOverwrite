b5419c9cf15ab74dafd34bbe7423cf776b9a390d
hrtowii <68852354+hrtowii@users.noreply.github.com>
2023-08-01 01:02:50 +0800

    try uicaching?

---
+++ WDBFontOverwrite/uicache/ISIconCacheServiceProtocol-Protocol.h
@@ -0,0 +1,12 @@
+//
+//  ISIconCacheServiceProtocol-Protocol.h
+//  WDBFontOverwrite
+//
+//  Created by LL on 1/8/23.
+//
+
+#ifndef ISIconCacheServiceProtocol_Protocol_h
+#define ISIconCacheServiceProtocol_Protocol_h
+
+
+#endif /* ISIconCacheServiceProtocol_Protocol_h */
